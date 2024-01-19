8d76e18ae35cafbb4d73e15092f725cd70a10a28
unknown <biswajitdeori9@gmail.com>
2024-01-19 15:12:35 +0530

    add

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,6 +1,6 @@
 const userModel  = require('../Models/userModel')
 
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 const jwt =  require('jsonwebtoken')
 
 const validator = require("validator")
